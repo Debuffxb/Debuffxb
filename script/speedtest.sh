7a673e5a40eec90a3c7fad1cf4556b690e9558ec
Debuffxb <m15858493948@163.com>
2020-12-02 15:00:06 +0800

    add speedtest

--- script/speedtest.sh
+++ script/speedtest.sh
@@ -4,3 +4,21 @@ rm speedtest
 fi
 echo "开始下载 speedtest"
 wget -q "https://raw.githubusercontent.com/Debuffxb/Debuffxb/main/bin/speedtest"
+echo "添加 systemd 文件"
+echo `[Unit]
+Description=speedtest service
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+ExecStart=/usr/local/speedtest/speedtest
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target` > '/etc/systemd/system/speedtest.service'
+echo "添加防火墙规则"
+iptables -I INPUT -p tcp --dport 15239 -j ACCEPT
+echo "启动进程"
+systemctl enable speedtest
+service speedtest start
