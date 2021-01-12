db2de703897ed168443ce77280d176961cc2e519
栗山未来 <37704441+Debuffxb@users.noreply.github.com>
2021-01-12 18:27:22 +0800

    Update qBittorrentInstaller2.sh

--- script/qBittorrentInstaller2.sh
+++ script/qBittorrentInstaller2.sh
@@ -3,7 +3,7 @@ apt update
 apt -y upgrade
 apt install -y sudo htop screen iftop sysstat
 apt install -y build-essential pkg-config automake libtool git zlib1g-dev libssl-dev libgeoip-dev libboost-dev libboost-system-dev libboost-chrono-dev libboost-random-dev
-apt install -y qtbase5-dev qttools5-dev libqt5svg5-dev libtorrent-dev
+apt install -y qtbase5-dev qttools5-dev libqt5svg5-dev libtorrent-dev libtorrent-rasterbar-dev
 git clone https://github.com/qbittorrent/qBittorrent
 cd qBittorrent
 git checkout release-4.2.5
