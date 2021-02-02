dcd0ceecd89cb163336eb99e6a95586021ff1b31
栗山未来 <37704441+Debuffxb@users.noreply.github.com>
2021-02-02 20:26:12 +0800

    Update deluge_2_0_3_Installer.sh

--- script/deluge_2_0_3_Installer.sh
+++ script/deluge_2_0_3_Installer.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 apt update
 apt -y upgrade
 apt install -y git gcc
@@ -9,3 +10,31 @@ cd deluge
 git reset b711cd258ae3001d5f1aa69617c22a2d258b1c06
 python setup.py build
 sudo python setup.py install --install-layout=deb
+echo 添加deluged.service
+echo '[Unit]
+Description=deluged
+After=network.target
+
+[Service]
+User=root
+Type=forking
+RemainAfterExit=yes
+LimitNOFILE=100000
+ExecStart=/usr/bin/deluged
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/deluged.service
+echo 添加deluge-web.service
+echo '[Unit]
+Description=deluge-web
+After=network.target
+
+[Service]
+User=root
+Type=forking
+RemainAfterExit=yes
+LimitNOFILE=100000
+ExecStart=/usr/bin/deluge-web -f
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/deluge-web.service
