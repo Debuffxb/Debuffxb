fa683b423a7cf7ebcbd0db9181903f4b78aa62ff
栗山未来 <37704441+Debuffxb@users.noreply.github.com>
2021-02-11 21:28:35 +0800

    Update deluge_2_0_3_Installer.sh

--- script/deluge_2_0_3_Installer.sh
+++ script/deluge_2_0_3_Installer.sh
@@ -3,8 +3,18 @@ apt update
 apt -y upgrade
 apt install -y git gcc
 apt install -y python python-twisted python-openssl python-setuptools intltool python-xdg python-chardet geoip-database python-libtorrent python-notify python-pygame python-glade2 librsvg2-common xdg-utils python-mako
-apt install -y libtorrent-rasterbar-dev python-pip  build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev python-dev
+apt install -y python-pip  build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev python-dev
 pip install pillow
+git clone https://github.com/arvidn/libtorrent.git
+cd libtorrent
+git checkout libtorrent-1_2_5
+./autotool.sh
+./configure --disable-debug --enable-encryption CXXFLAGS="-std=c++14"
+make clean && make -j$(nproc)
+make install
+ln -s /usr/local/lib/libtorrent-rasterbar.so.10 /usr/lib/libtorrent-rasterbar.so.10
+cd ../
+rm libtorr* -r
 git clone git://deluge-torrent.org/deluge.git
 cd deluge
 git reset b711cd258ae3001d5f1aa69617c22a2d258b1c06
